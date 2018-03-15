a39fe44c4a653614275cc34fecc5ec0294249999
nickgarlis <nickgarlis@gmail.com>
2018-03-15 21:32:45 +0200

    scholardi source code

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Konstantinos Chorianopoulos",
+        "excerpt":"PublicationsGarneli, V., &amp; Chorianopoulos, K. (2018). Programming video games and simulations in science education: exploring computational thinking through code analysis. Interactive Learning Environments, 1–16. https://doi.org/10.1080/10494820.2017.1337036Merkouris, A., Chorianopoulos, K., &amp; Kameas, A. (2017). Teaching Programming in Secondary Education Through Embodied Computing Platforms: Robotics and Wearables. ACM Transactions on Computing Education, 17(2),...","categories": ["HILab"],
+        "tags": ["Human-Computer Interaction","Multimedia","Software Engineering","Computer-Supported Collaborative Work"],
+        "url": "/scholars/choko/",
+        "teaser":null}]
